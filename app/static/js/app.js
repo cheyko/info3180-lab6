14a4d2f9b964efa109e22cfac75fd0058617b8ed
ariel wilson <ariel.wilson@mymona.uwi.edu>
2018-03-25 11:10:31 +0000

    API wasnt working for a while plus finally reach routing part

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -24,6 +24,61 @@ Vue.component('app-header', {
     data: function() {}
 });
 
+Vue.component('news-list',{
+    template: `
+    <div class="news">
+        <h2>News</h2>
+        <ul class="news__list">
+            <li v-for="article in articles" class="news__item">{{ article.title }} </li>   
+        </ul>
+    </div>
+    <div class="form-inline d-flex justify-content-center">
+ <div class="form-group mx-sm-3 mb-2">
+ <label class="sr-only" for="search">Search</label>
+ <input type="search" name="search" v-model="searchTerm"
+id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+search term here" />
+ <button class="btn btn-primary mb-2"
+@click="searchNews">Search</button>
+ </div>
+</div>`,
+    created: function() 
+    {
+        let self = this;
+        
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=7ea93f6878f64679823d00088d8d327d')
+            .then(function(response){  
+                return response.json();           
+                })           
+            .then(function(data){ 
+                    console.log(data);
+                    self.articles = data.articles;        
+                });     
+    },
+    data: function()
+    {  
+        return {
+            articles: []
+            searchTerm: '' 
+        }     
+    } },
+ methods: {
+ searchNews: function() {
+ let self = this;
+ fetch('https://newsapi.org/v2/everything?q='+
+self.searchTerm + '&language=en&apiKey=7ea93f6878f64679823d00088d8d327d')
+ .then(function(response) {
+ return response.json();
+ })
+ .then(function(data) {
+ console.log(data);
+ self.articles = data.articles;
+ });
+ }
+ } 
+    
+});
+
 Vue.component('app-footer', {
     template: `
         <footer>
